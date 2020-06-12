d996202664d6c9568e727582dbd82327151d0b01
InasaXia <xiayangzzw@gmail.com>
2020-06-12 14:08:53 +0800

    nothing

--- ClientStream/Client/main/Client.go
+++ ClientStream/Client/main/Client.go
@@ -9,14 +9,14 @@ import (
 )
 
 func main() {
-	conn,err := grpc.Dial("127.0.0.1:8080",grpc.WithInsecure())
+	conn,err := grpc.Dial("rhel:8081",grpc.WithInsecure())
 	if err!=nil {
 		log.Println(err)
 	}
 	defer conn.Close()
 	c := services.NewHelloServiceClient(conn)
 	stream,_ := c.HelloServiceStream(context.Background())
-	for i:=0;i<10;i++ {
+	for i:=0;i<1000;i++ {
 		stream.Send(&services.Request{Msg: "hello from client"})
 		time.Sleep(time.Second)
 	}
